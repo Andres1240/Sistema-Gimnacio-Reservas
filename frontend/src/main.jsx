9846b27a52019dc7ff508381708170433baf13b0
Andres coronado <andrescoronado1842@hotmail.com>
2026-05-18 18:14:32 -0500

    adicion de estilos para vistas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/theme.css';
+
 import { AuthProvider }
 from './context/AuthContext';
 
